87d0b37a58091bba2098e8330299be4c52149334
Lichen Ma <lichenma123@gmail.com>
2021-06-12 12:09:18 -0400

    updated java implementation

--- Java/maze.java
+++ Java/maze.java
@@ -1,12 +1,48 @@
 import java.util.*; 
 
 public class maze {
+	public static void parseInputs(String input) throws Exception{
+		String[] inputArr = input.split("\\n");
+		if (inputArr.length<3){
+			throw new Exception("Unexpected number of inputs");
+		}
+
+		int[][] maze = generateMaze(inputArr);
+		String[] robotCoord = inputArr[1].split(" ");
+		String[] userCoord = inputArr[2].split(" ");
+		int robotX = Integer.valueOf(robotCoord[1]); 
+		int robotY = Integer.valueOf(robotCoord[0]); 
+		int userX = Integer.valueOf(userCoord[1]); 
+		int userY = Integer.valueOf(userCoord[0]); 
+		System.out.println("\nA* traversal"); 
+		if (inputArr.length > 4){
+			System.out.println("Agent starting at Robot, picking up Item and ending at User\n");
+			// we have an item to pick up 
+			String[] itemCoord = inputArr[4].split(" ");
+			int itemX = Integer.valueOf(itemCoord[1]);
+			int itemY = Integer.valueOf(itemCoord[0]);
+
+			// for first iteration we make user an obstacle and traverse from robot to item
+			maze[userX][userY] = 1;
+			Node tmp = aStar(robotX, robotY, itemX, itemY, maze, "", -1, false);
+
+			tmp.path += "-> Picked Up Item "; 
+			// then we traverse from item to user continuing values from first traversal 
+			maze[userX][userY] = 0;
+			aStar(itemX, itemY, userX, userY, maze, tmp.path, tmp.cost-1, true);
+		} else { 
+			System.out.println("Agent starting at Robot and ending at User\n");
+			aStar(robotX, robotY, userX, userY, maze, "", -1, true);
+		}
+	}
 	/**
-	  * Helper method to generate the example maze provided in the assignment - represented as an array of integers with 1 representing a blocked square and 0 representing empty
+	  * Helper method to generate input maze - represented as an array of integers with 1 representing a blocked square and 0 representing empty
+	  * 
+	  * @param inputArr input array split by line
 	  */
 	public static int[][] generateMaze(String[] inputArr){
 		String[] arrayDimensions = inputArr[0].split(" ");
-		int[][] maze = new int[Integer.valueOf(arrayDimensions[0])][Integer.valueOf(arrayDimensions[1])];
+		int[][] maze = new int[Integer.valueOf(arrayDimensions[1])][Integer.valueOf(arrayDimensions[0])];
 
 		// we know there are obstacles
 		if (inputArr.length>3){
@@ -35,7 +71,7 @@ public class maze {
 	/*
 	 * A* search algorithm using priority queue and manhattan distance for heuristic 
 	 */ 
-	public static void aStar(int sx, int sy, int ex, int ey, int[][] maze){
+	public static Node aStar(int sx, int sy, int ex, int ey, int[][] maze, String initPath, int initCost, boolean printResult){
 		int[][] dirs = new int[][]{{0,1}, {0,-1},{1,0},{-1,0}};
 		int explored = 0;
 		HashSet<String> visited = new HashSet<>();
@@ -47,7 +83,7 @@ public class maze {
 			int dyB = Math.abs(b.y - ey); 
 			return (dxA + dyA + a.cost) - (dxB + dyB + b.cost); 
 		}); 
-		pq.offer(new Node(sx, sy, "", 0));
+		pq.offer(new Node(sx, sy, initPath, initCost));
 		while(!pq.isEmpty()){
 			Node curr = pq.poll();
 			if (visited.contains(curr.x + "," + curr.y)){
@@ -55,16 +91,18 @@ public class maze {
 			}			
 
 			// update current path and current cost  
-			curr.path += "-> (" + curr.x + "," + curr.y + ") "; 
+			curr.path += ", (" + curr.y + "," + curr.x + ")"; 
 			curr.cost++;
 			explored++;
 
 			// if coordinates are that of end point - we have found the destination and can print the current path/cost and exit	
 			if (curr.x == ex && curr.y == ey){
-				System.out.println("Complete path " + curr.path); 
-				System.out.println("Cost: " + curr.cost); 
-				System.out.println("Number of explored Nodes: " + explored); 
-				return;
+				if (printResult){
+					System.out.println("Complete path " + curr.path.substring(1)); 
+					System.out.println("Cost: " + curr.cost); 
+					// System.out.println("Number of explored Nodes: " + explored); 
+				}
+				return curr;
 			}
 
 			for (int[] dir:dirs){
@@ -78,6 +116,7 @@ public class maze {
 			// add current node to visited set
 			visited.add(curr.x + "," + curr.y); 	
 		}
+		return null;
 	}
 	
 	/**
@@ -85,47 +124,50 @@ public class maze {
 	 * @throws Exception
 	  */
 	public static void main(String[] args) throws Exception{
-		// String input = new StringBuilder()
-		// 	.append("3 3\n")
-		// 	.append("0 0\n")
-		// 	.append("2 2\n")
-		// 	.toString();
+		List<String> testCases = new ArrayList<>();
+		
+		// input 1
+		String input1 = new StringBuilder()
+			.append("3 3\n")
+			.append("0 0\n")
+			.append("2 2\n")
+			.toString();
+		testCases.add(input1);
 
-		String input = new StringBuilder()
+		// input 2
+		String input2 = new StringBuilder()
+			.append("4 4\n")
+			.append("3 3\n")
+			.append("0 1\n")
+			.append("2 1 1 3\n")
+			.toString();
+		testCases.add(input2);
+
+		// input 3
+		String input3 = new StringBuilder()
 			.append("6 6\n")
 			.append("1 1\n")
 			.append("4 4\n")
 			.append("2 0 3 1 2 2 1 2 1 4\n")
 			.toString();
+		testCases.add(input3);
 
-		String[] inputArr = input.split("\\n");
+		// input 4
+		String input4 = new StringBuilder()
+			.append("6 8\n")
+			.append("2 6\n")
+			.append("3 5\n")
+			.append("2 1 4 2 1 3 3 3 1 5 1 6 2 5 4 5 4 6\n")
+			.append("3 2\n")
+			.toString();
+		testCases.add(input4);
 
-		if (inputArr.length<3){
-			throw new Exception("Unexpected number of inputs");
-		}
-
-		int[][] maze;
-		try {
-			maze = generateMaze(inputArr);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-
-		String[] robotCoord = inputArr[1].split(" ");
-		String[] userCoord = inputArr[2].split(" ");
-		
-		int startX = Integer.valueOf(robotCoord[0]); 
-		int startY = Integer.valueOf(robotCoord[1]); 
-		int endX = Integer.valueOf(userCoord[0]); 
-		int endY = Integer.valueOf(userCoord[1]);  
-	
-
-		System.out.println("\nA* traversal"); 
-		System.out.println("Agent starting at S and ending at E1\n");
-		aStar(startX, startY, endX, endY, maze);
+		Scanner scanner = new Scanner(System.in);
+		int inputSelection;
+		System.out.println("Enter the test case you would like to test (1-4) or any other number to quit:");
+        while((inputSelection = scanner.nextInt()) > 0 && inputSelection<5) {
+            parseInputs(testCases.get(inputSelection-1));
+			System.out.println("Please enter another test case you would like to test (1-4) or any other number to quit:");
+        }
 	}
-
-	
 }
